a873bee469bd3099a6613fc2f3a39ca9a92af1d2
Erick Mireles <emireles.rosas@gmail.com>
2026-01-11 21:45:29 -0500

    feat: add account bar to balance

--- packages/web/src/components/monthlyBalance.svelte
+++ packages/web/src/components/monthlyBalance.svelte
@@ -2,31 +2,46 @@
 	import { getUserAccounts } from '../context';
 
 	const currentUserAccounts = getUserAccounts();
+	const totalBalance = $derived(
+		currentUserAccounts.data?.reduce((acc, account) => acc + account.balance, 0)
+	);
 </script>
+
+{#snippet balanceIndicator(balance: number, account: string)}
+	{#if balance > 0}
+		<div class="group inline-flex flex-col gap-1 overflow-clip" style={`flex-basis: ${balance}%;`}>
+			<div class="h-1 w-full rounded-full group-odd:bg-secondary group-even:bg-primary"></div>
+			<span
+				class="shrink-0 text-[10px] text-nowrap group-odd:text-secondary group-even:text-primary"
+				>{account}</span
+			>
+		</div>
+	{/if}
+{/snippet}
 
 <div class="border-light-alt flex flex-col gap-1 border-y bg-card px-4 pt-24 pb-6">
 	{#if currentUserAccounts.isLoading}
 		<h2 class="skeleton bg-clip-text text-5xl font-semibold text-transparent">$0</h2>
-		<span class="mt-1 text-xs opacity-50">Loading Balance...</span>
-		<div class="skeleton mt-2 h-1 w-full rounded-full"></div>
+		<span class="mb-2 text-xs opacity-50">Loading Balance...</span>
+		<div class="inline-flex flex-1 flex-col gap-1">
+			<div class="skeleton h-1 w-full rounded-full"></div>
+			<span class="text-[10px] opacity-50">Account</span>
+		</div>
 	{:else if currentUserAccounts.error}
 		<p>Error: {currentUserAccounts.error.toString()}</p>
-	{:else}
+	{:else if currentUserAccounts}
 		<h2 class="text-5xl font-semibold">
-			{currentUserAccounts?.data
-				?.reduce((acc, account) => acc + account.balance, 0)
-				.toLocaleString('en-US', {
-					style: 'currency',
-					currency: 'USD',
-					maximumFractionDigits: 0
-				})}
+			{totalBalance?.toLocaleString('en-US', {
+				style: 'currency',
+				currency: 'USD',
+				maximumFractionDigits: 0
+			})}
 		</h2>
-		<span class="mt-1 text-xs">Current Balance</span>
-		<div class="mt-2">
-			<div>
-				<div class="h-1 w-full rounded-full bg-green-800"></div>
-				<span class="text-[10px] text-green-800">Cash</span>
-			</div>
+		<span class="mb-2 text-xs">Current Balance</span>
+		<div class="flex gap-0.5">
+			{#each currentUserAccounts.data as account (account._id)}
+				{@render balanceIndicator(account.balance, account.name)}
+			{/each}
 		</div>
 	{/if}
 </div>
